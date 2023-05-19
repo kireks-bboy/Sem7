3bdd7d2c58f352648acf89b8ddaa0aec5883da4a
Kirill Denisevitch <denviko00@gmail.com>
2023-05-19 22:08:31 +0300

    semsev

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+// 1
+// const dropEl = document.querySelectorAll('.dropdown-item')
+// console.log(dropEl)
+// dropEl.forEach(element => {
+//   element.classList.toggle('super-dropdown')
+// });
+
+
+// 2
+// const btn = document.querySelector('.btn');
+// if (btn.classList.contains('btn-secondary')) {
+//   btn.classList.remove('btn-secondary')
+// }
+// else{
+//   btn.classList.add('btn-secondary')
+// }
+
+
+// 3
+
+// const menuEl = document.querySelector('.menu');
+// menuEl.classList.remove('dropdown-menu');
+
+
+// 4
+
+
+// const dropEl = document.querySelector('div.dropdown');
+// dropEl.insertAdjacentHTML("afterend", `<a href="#">link</a>`);
+
+
+// 5
+
+// const elID = document.getElementById('dropdownMenuButton');
+// elID.id = 'superDropDown'
+
+
+// 6 
+
+// const atribEl = document.querySelector('[aria-labelledby="dropdownMenuButton"]')
+// atribEl.dataset.dd = "3";
+
+
+// 7 
+
+// const typeEl = document.querySelector('.dropdown-toggle');
+// typeEl.removeAttribute('type')
+
